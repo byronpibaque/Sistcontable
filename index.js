b051dba1e45771e2a68afc07d90ee6a6e424561b
BYRON PIBAQUE <enmanuelpibaque@outlook.es>
2022-04-05 11:30:26 -0500

    pr

--- index.js
+++ index.js
@@ -1,53 +1,60 @@
-import express from 'express';
+import express from "express";
 
-import morgan from 'morgan';
+import morgan from "morgan";
 
-import cors from 'cors';
+import cors from "cors";
 
-import path from 'path';
+import path from "path";
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-import router from './routes';
+import router from "./routes";
 
 //Conexión a la base de datos MongoDB
-mongoose.Promise=global.Promise;
+mongoose.Promise = global.Promise;
 // const dbUrl = 'mongodb://localhost:27017/dbSistemas';
 
-const dbUrl = 'mongodb+srv://byron:Byron9009@sistemaventas-crznc.mongodb.net/sistemacontable?retryWrites=true&w=majority';
-mongoose.connect(dbUrl, {useCreateIndex:true, useNewUrlParser: true,useUnifiedTopology: true})
-.then(mongoose => console.log('Conexion satisfactoria con la base de datos.'))
-.catch(err => console.log(err));
+const dbUrl =
+    "mongodb+srv://byron:Byron9009@sistemaventas-crznc.mongodb.net/sistemacontable?retryWrites=true&w=majority";
+mongoose
+    .connect(dbUrl, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then((mongoose) =>
+        console.log("Conexion satisfactoria con la base de datos.")
+    )
+    .catch((err) => console.log(err));
 
+const app = express();
 
-const app=express();
+app.use(morgan("dev"));
 
-
-app.use(morgan('dev'));
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-if (process.env.NODE_ENV === "production"){
-    app.use('/api',router);
-    app.set('port',process.env.PORT || 777);
-    app.listen(
-        app.get('port'),()=>{
-            console.log('Servidor de NojdeJS levantado en el puerto: ' + app.get('port'));
-        });
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,  "public", "index.html"));
+if (process.env.NODE_ENV === "production") {
+    app.use("/api", router);
+    app.set("port", process.env.PORT || 777);
+    app.listen(app.get("port"), () => {
+        console.log(
+            "Servidor de NojdeJS levantado en el puerto: " + app.get("port")
+        );
     });
-}else{
-    app.use('/api',router);
-    app.set('port',process.env.PORT || 35000);
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "public", "index.html"));
+    });
+} else {
+    app.use("/api", router);
+    app.set("port", process.env.PORT || 35000);
 
-    app.listen(
-        app.get('port'),()=>{
-            console.log('Servidor de NojdeJS levantado en el puerto: ' + app.get('port'));
-        });
+    app.listen(app.get("port"), () => {
+        console.log(
+            "Servidor de NojdeJS levantado en el puerto: " + app.get("port")
+        );
+    });
 }
-
